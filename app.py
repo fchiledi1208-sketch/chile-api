d96f298f3bbce15bafba5580c1dac184b82a789d
fchiledi1208-sketch <f.chiledi1208@gmail.com>
2026-06-08 12:29:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "°Chile...`D API iko online!"
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    data = request.get_json()
+    message = data.get("message", "")
+
+    return jsonify({
+        "reply": "Umesema: " + message
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
